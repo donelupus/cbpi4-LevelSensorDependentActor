003d2c7c81d54239f682d5ce3b927469a96cda78
Done_Wolf <anton.wolf3@gmail.com>
2025-10-20 22:00:20 +0200

    Make GPIODependentActor depend on 2 GPIO sensors

--- cbpi4-GPIODependentActor/__init__.py
+++ cbpi4-GPIODependentActor/__init__.py
@@ -13,29 +13,38 @@ import RPi.GPIO as GPIO
 logger = logging.getLogger(__name__)
 
 @parameters([Property.Actor(label="Actor",  description="Select the actor that will be switched off depending on the GPIO state."),
-            Property.Select(label="GPIO", options=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27], description="GPIO [BMC numbering] that will switch off the Actor"), 
-            Property.Select(label="GPIOstate", options=["High", "Low"],description="High: Actor switches off on GPIO high; Low: Actor switches off on GPIO low"),
+            Property.Select(label="GPIO_Upper", options=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27], description="GPIO [BMC numbering] of upper Level Sensor"), 
+            Property.Select(label="GPIO_Lower", options=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27], description="GPIO [BMC numbering] of lower Level Sensor"), 
+            Property.Select(label="GPIOstate", options=["High", "Low"],description="High: Actor switches off when both GPIOstates are high; Low: Actor switches off when both GPIOstates are  low"),
             Property.Select(label="notification", options=["Yes", "No"], description="Will show notification when GPIO switches actor off")])
 
 class GPIODependentActor(CBPiActor):
 
     async def wait_for_input(self):
         while True:
-            inputsignal=GPIO.input(int(self.ActorDependency))
-            logging.info(inputsignal)
-            if (self.dependency_type == "High") and (GPIO.input(int(self.ActorDependency))):
-                logging.info("Break on high")
-                await self.off()
-                self.state = False
-                if self.notification == "Yes":
-                    self.cbpi.notify("GPIO Dependent Actor", "Actor {} switched off as GPIO {} siwtchted to {}".format(self.name, self.ActorDependency, self.dependency_type), NotificationType.INFO)
+            input_signal_upper=GPIO.input(int(self.gpio_upper))
+            input_signal_lower=GPIO.input(int(self.gpio_lower))
+            logging.error("GPIODependentActor: wait_for_input: input_signal_upper: %s and input_signal_lower: %s", input_signal_upper, input_signal_lower)
+            if (self.dependency_type == "High"):
+                if (GPIO.input(int(self.gpio_upper))) and (GPIO.input(int(self.gpio_lower))):
+                    logging.error("GPIODependentActor: Start on both high")
+                    await self.on()
+                    self.state = False
+                    if self.notification == "Yes":
+                        self.cbpi.notify("GPIO Dependent Actor", "Actor {} switched on as lower GPIO {} and upper GPIIO {} siwtchted to {}".format(self.name, self.gpio_lower, self.gpio_upper, self.dependency_type), NotificationType.error)
+                elif (not GPIO.input(int(self.gpio_upper))) and not (GPIO.input(int(self.gpio_lower))):
+                    logging.error("GPIODependentActor: Stop on both low")
+                    await self.off()
+                    self.state = False
+                    if self.notification == "Yes":
+                        self.cbpi.notify("GPIO Dependent Actor", "Actor {} switched on as lower GPIO {} and upper GPIIO {} siwtchted to {}".format(self.name, self.gpio_lower, self.gpio_upper, self.dependency_type), NotificationType.error)
                 break
-            elif (self.dependency_type == "Low") and not (GPIO.input(int(self.ActorDependency))):
-                logging.info("Break on Low")
+            elif (self.dependency_type == "Low") and not (GPIO.input(int(self.gpio_upper))) and not (GPIO.input(int(self.gpio_lower))):
+                logging.error("GPIODependentActor: Break on Low")
                 await self.off()
                 self.state = False
                 if self.notification == "Yes":
-                    self.cbpi.notify("GPIO Dependent Actor", "Actor {} switched off as GPIO {} siwtchted to {}".format(self.name, self.ActorDependency, self.dependency_type), NotificationType.INFO)
+                    self.cbpi.notify("GPIO Dependent Actor", "Actor {} switched off as GPIO {} siwtchted to {}".format(self.name, self.gpio_upper, self.dependency_type), NotificationType.error)
                 break
             if self.interrupt == True:
                 break
@@ -48,37 +57,43 @@ class GPIODependentActor(CBPiActor):
             self.name = (self.cbpi.actor.find_by_id(self.base).name)
         except:
             self.name = ""
-        self.ActorDependency = self.props.get("GPIO", None)
+        self.gpio_upper = self.props.get("GPIO_Upper", None)
+        self.gpio_lower = self.props.get("GPIO_Lower", None)
         self.dependency_type = self.props.get("GPIOstate", "High")
         self.notification = self.props.get("notification", "Yes")
         self.interrupt = False
         mode = GPIO.getmode()
-        logging.info(mode)
+        logging.error("GPIODependentActor: on_start GPIO mode: %s", mode)
         if (mode == None):
             GPIO.setmode(GPIO.BCM)
-        if self.ActorDependency is not None:
-            GPIO.setup(int(self.ActorDependency), GPIO.IN)
+        if self.gpio_upper is not None:
+            GPIO.setup(int(self.gpio_upper), GPIO.IN)
+        if self.gpio_lower is not None:
+            GPIO.setup(int(self.gpio_lower), GPIO.IN)
         else:
             pass
 
         pass
 
     async def on(self, power=0):
+        logging.error("GPIODependentActor: on")
         self.interrupt = False
         await self.cbpi.actor.on(self.base)
         self._task = asyncio.create_task(self.wait_for_input())
         self.state = True
 
     async def off(self):
-        logger.info("ACTOR %s OFF " % self.base)
+        logging.error("ACTOR %s OFF " % self.base)
         await self.cbpi.actor.off(self.base)
         self.interrupt = True
         self.state = False
 
     def get_state(self):
+        logging.error("GPIODependentActor: get_state: %s", self.state)
         return self.state
     
     async def run(self):
+        logging.error("GPIODependentActor: run")
         pass
 
 
